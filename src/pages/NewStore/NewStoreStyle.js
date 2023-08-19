64a1a12a83dcaec531f6ae15b191a3088d3634b5
dan-2ee <ekdms7273@naver.com>
2023-08-20 05:24:10 +0900

    버튼을 왜 한번더 감싸서 사용했을까.....

--- src/pages/NewStore/NewStoreStyle.js
+++ src/pages/NewStore/NewStoreStyle.js
@@ -5,23 +5,18 @@ export const StoreName1 = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 20px;
-`;
-
-export const SaveButtonDiv = styled.div`
-  margin-top: 2%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  font-size: 21px;
 `;
 
 export const SaveButton = styled.button`
-  width: 100px;
-  height: 40px;
+  width: 115px;
+  height: 45px;
   background-color: #98c597;
   border: none;
   color: white;
   border-radius: 10px;
   cursor: pointer;
   font-family: "Yeongdeok Snow Crab";
+  font-size: 15px;
+  margin-top: 5%;
 `;
